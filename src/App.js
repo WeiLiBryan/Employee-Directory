dfd8203a29414ffb48339b52a9dfead597c2a753
William Bryan <wwbryan00@gmail.com>
2021-02-26 02:10:45 -0800

    add a use effect function to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import API from "./utils"
 import Hero from "./components/Hero";
 import Search from "./components/Search";
 import Table from "./components/Table"
 
 function App() {
+
+  const [emps, setEmps] = useState([]);
+  const [getSearch, setSearch] = useState("");
+
+  userEffect(() => {loadEmps()}, []);
+
+  loadEmps = () => {
+    API.findUsers()
+      .then(res => {
+        setEmps(res);
+      });
+  } 
+
+
   return (
     <div>
       <Hero />
